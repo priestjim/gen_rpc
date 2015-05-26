3e7689c8739a84b03ac9ee4ee75ba415879562cf
Panagiotis Papadomitsos <pj@ezgr.net>
2015-05-26 16:13:01 -0700

    Added lager as a dependency. Some beautification

--- src/gen_rpc.erl
+++ src/gen_rpc.erl
@@ -1,26 +1,23 @@
-%%%-------------------------------------------------------------------
-%% @doc gen_rpc public API
-%% @end
-%%%-------------------------------------------------------------------
+%%% -*-mode:erlang;coding:utf-8;tab-width:4;c-basic-offset:4;indent-tabs-mode:()-*-
+%%% ex: set ft=erlang fenc=utf-8 sts=4 ts=4 sw=4 et:
+%%%
+%%% Copyright 2015 Panagiotis Papadomitsos, Inc. All Rights Reserved.
+%%%
 
 -module(gen_rpc).
+-author("Panagiotis Papadomitsos <pj@ezgr.net>").
 
+%%% Behaviour
 -behaviour(application).
 
-%% Application callbacks
+%%% Application callbacks
 -export([start/2, stop/1]).
 
-%%====================================================================
-%% API
-%%====================================================================
-
+%%% ===================================================
+%%% Application callbacks
+%%% ===================================================
 start(_StartType, _StartArgs) ->
     gen_rpc_sup:start_link().
 
-%%--------------------------------------------------------------------
 stop(_State) ->
     ok.
-
-%%====================================================================
-%% Internal functions
-%%====================================================================
