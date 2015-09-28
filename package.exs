e5f8d48e37ed2cb2ee8770fddc19de4f4055ad0a
Panagiotis Papadomitsos <pj@ezgr.net>
2015-09-27 17:10:05 -0700

    Updated project information

--- package.exs
+++ package.exs
@@ -3,13 +3,13 @@ defmodule FS.Mixfile do
 
   def project do
     [app: :gen_rpc,
-     version: "0.1.0",
-     description: "A Elixir wrapper of gen_rpc library for out-of-band messaging",
+     version: "1.0.0",
+     description: "An Erlang RPC library for out-of-band messaging",
      package: package]
   end
 
   defp package do
-    [files: ~w(c_src include priv src LICENSE Makefile package.exs README.md rebar.config),
+    [files: ~w(include src LICENSE Makefile package.exs README.md TODO.md rebar.config),
      contributors: ["priestjim"],
      licenses: ["Apache 2.0"],
      links: %{"GitHub" => "https://github.com/priestjim/gen_rpc"}]
