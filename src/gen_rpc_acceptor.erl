1c43f73b74b508f0b017c4eeecec648a78d82675
root <root@myserver.domain.com>
2015-10-06 08:18:42 -0700

    throw shows up same as error, not ignored. Fix test

--- src/gen_rpc_acceptor.erl
+++ src/gen_rpc_acceptor.erl
@@ -197,9 +197,15 @@ call_worker(Parent, WorkerPid, Ref, M, F, A) ->
     % and manifest as timeout. Wrap inside anonymous function with catch
     % will crash the worker quickly not manifest as a timeout.
     % See call_MFA_undef test.
-    Ret = case catch erlang:apply(M, F, A)of
-               {'EXIT', _} = V -> {badrpc, V};
-               Else -> Else
+   % Ret = case catch erlang:apply(M, F, A) of
+   %            {'EXIT', _} = V -> {badrpc, V};
+   %            Else -> Else
+   %       end,
+    Ret = try erlang:apply(M, F, A) 
+          catch 
+               throw:Term -> {badrpc, {'EXIT', Term}};
+               exit:Reason -> {badrpc, {'EXIT', Reason}};
+               error:Reason -> {badrpc, {'EXIT', Reason, erlang:get_stacktrace()}}
           end,
     PacketBin = erlang:term_to_binary({WorkerPid, Ref, Ret}),
     Parent ! {call_reply, PacketBin},
