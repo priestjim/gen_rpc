06c4917c7a5ae0ac90f713dbd48351b36d4b3a2e
root <root@myserver.domain.com>
2015-10-05 23:27:15 -0700

    Revert "spacing and use try/catch"
    This reverts commit 4470e2d09a324941fac13ef11b0d6a4db89a9de6.

--- src/gen_rpc_acceptor.erl
+++ src/gen_rpc_acceptor.erl
@@ -197,11 +197,9 @@ call_worker(Parent, WorkerPid, Ref, M, F, A) ->
     % and manifest as timeout. Wrap inside anonymous function with catch
     % will crash the worker quickly not manifest as a timeout.
     % See call_MFA_undef test.
-    Ret = try erlang:apply(M, F, A)
-          catch
-               throw:Term -> {badrpc, Term};
-               exit:Reason -> {badrpc, Reason};
-               error:Reason -> {badrpc, {'EXIT',{Reason, erlang:get_stacktrace()}}}
+    Ret = case catch erlang:apply(M, F, A)of
+               {'EXIT', _} = V -> {badrpc, V};
+               Else -> Else
           end,
     PacketBin = erlang:term_to_binary({WorkerPid, Ref, Ret}),
     Parent ! {call_reply, PacketBin},
